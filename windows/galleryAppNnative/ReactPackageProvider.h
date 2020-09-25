8ba7367e08c059cde0202a0a67a6afd42f00e899
TheLogan <timlogan475@gmail.com>
2020-09-25 14:30:43 +0200

    initial commit

---
+++ windows/galleryAppNnative/ReactPackageProvider.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "winrt/Microsoft.ReactNative.h"
+
+
+namespace winrt::galleryAppNnative::implementation
+{
+    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
+    {
+    public: // IReactPackageProvider
+        void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
+    };
+} // namespace winrt::galleryAppNnative::implementation
+
+
